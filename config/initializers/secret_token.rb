812f2846aba24d601163665fbdd786dca9f6faf9
Shane Lee <admin@innatelycurious.com>
2013-12-07 13:17:25 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conspire::Application.config.secret_key_base = '4134ea2270df5483896a01f8565a23154c535245270cd330a2e9caf79ce997cb3ae4ea7898c0260ffd5f7df0acbd294faa98d893118c83b5a6adcc690883ad4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conspire::Application.config.session_store :cookie_store, key: '_conspire_session'
