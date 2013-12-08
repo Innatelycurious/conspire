0c93feca8f2d7d0254799968173823fc2b204c7e
Shane Lee <admin@innatelycurious.com>
2013-12-08 17:27:00 +0000

    Created home and team pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Conspire::Application.routes.draw do
+root "pages#home"
+
+get "team" => "pages#team"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
